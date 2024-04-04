dd8d7b7189301b533b3fa8173da430209cc5ac2c
chadvedar <lee.jira.natee@gmail.com>
2024-04-04 02:37:20 +0000

    add pot

--- main.cpp
+++ main.cpp
@@ -2,8 +2,19 @@
 #include "Servo.h"
 
 Servo myservo(D7);
- 
+AnalogIn Pot(A5);
+int val; 
+
+long map(long x, long in_min, long in_max, long out_min, long out_max)
+{
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+}
+
 int main() {
-    myservo = 0.0/100.0; // 0(0 deg) - 100(180 deg) value 
-    //ThisThread::sleep_for(10ms);
+    while (1){
+        val = map(Pot.read_u16(), 900, 65535, 0, 100);
+        myservo = val/100.0; // 0(0 deg) - 100(180 deg) value 
+        printf("%d\n", val);
+        ThisThread::sleep_for(10ms);
+    }
  }
